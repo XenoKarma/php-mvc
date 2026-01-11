a2cab4a4b858147dce0bb2cb29445f62d4e978f0
XenoKarma <andikapurnama1803@gmail.com>
2026-01-11 15:28:36 +0700

    day2

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+
+    public function index($nama = 'Andika', $pekerjaan = 'Gamer'){
+        echo "Halo, nama saya $nama, pekerjaan saya adalah $pekerjaan";
+    }
+
+    public function page(){
+        echo 'About/page';
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,8 +1,53 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
-        echo 'oke';
+        $url = $this->parseUrl();
+        
+        // didalam folder controller yang namanya sesuai dengan nama yang ada disini
+        // kita buat controller
+        if(isset($url[0])){
+        if(file_exists('../app/controllers/'. $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        }
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // untuk method
+        if(isset($url[1])){
+            // apakah ada methodnya didalam controller
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // params
+        if(!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        // jalankan controller dan method dan kirimkan params jika ada
+        // gunakan function call_user_func_array();
+        call_user_func_array([$this->controller, $this->method], $this->params);
+        
+    }
+
+    // membuat method
+    public function parseUrl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
+
